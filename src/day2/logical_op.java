08dcdffaab6eb8a88e903519ccb1d7605a5f1487
Vetrikanth1209 <vetree1209mec@gmail.com>
2025-07-10 12:58:21 +0530

    rel op

---
+++ src/day2/logical_op.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class logical_op {
+	public static void main(String[] args) {
+		int a=5,b=3,c=4;
+		System.out.println((a<b)&&(b>c));
+		System.out.println((a>b)||(b>c));
+		int num= 10;
+		System.out.println((num==10));
+		System.out.println(!(num==10));
+	}
+}
--- src/day2/relational_op.java
+++ src/day2/relational_op.java
@@ -1,5 +1,11 @@
 package day2;
 
 public class relational_op {
-
+	public static void main (String[] args) {
+		int a=10,b=20,c=5,d=10;
+		System.out.println(a>c);
+		System.out.println(a==c);
+		System.out.println(a<=d);
+		System.out.println(a==d);
+	}
 }
